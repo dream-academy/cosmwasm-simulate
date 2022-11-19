c6bfa7fad1eea46bfa22e7fc66588b289071bf9a
procfs <wireless@kaist.ac.kr>
2022-11-19 21:56:24 +0900

    add buildflags for llvm cov

--- rust-1.65.0/library/std/src/os/linux/net.rs
+++
@@ -1,4 +0,0 @@
-//! Linux and Android-specific definitions for socket options.
-
-#![unstable(feature = "tcp_quickack", issue = "96256")]
-pub use crate::os::net::tcp::TcpStreamExt;
--- rust-1.65.0/library/std/src/os/net/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Linux and Android-specific definitions for socket options.
-
-#![unstable(feature = "tcp_quickack", issue = "96256")]
-#![doc(cfg(any(target_os = "linux", target_os = "android",)))]
-pub mod tcp;
-#[cfg(test)]
-mod tests;
