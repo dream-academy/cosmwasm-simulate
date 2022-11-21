7ccab36ef1ddfe237d4b905e5b2aa451e3c12dde
procfs <wireless@kaist.ac.kr>
2022-11-21 15:51:55 +0900

    add test for mainnet

--- python-bindings/tests/test_mainnet.py
+++ python-bindings/tests/test_mainnet.py
@@ -4,5 +4,5 @@ import base64
 
 if __name__ == "__main__":
     RPC_URL = "http://51.81.155.97:26657"
-    m = Model("RPC", RPC_URL, None, "terra")
+    m = Model(RPC_URL, None, "terra")
     print(m)
